e25c42f124a9e52e1fafe07467437631cddcdd79
Yucao42 <cqcy3210@gmail.com>
2020-07-09 22:58:33 -0500

    stream in and out in c

--- ffmpeg/ffmpeg_image.cpp
+++ ffmpeg/ffmpeg_image.cpp
@@ -9,6 +9,7 @@
 #include <condition_variable>
 #include <opencv2/opencv.hpp>
 #include <sys/wait.h> 
+#include <stdlib.h> 
 #include "../timer.h"
 
 const std::string addr_prefix("tcp://localhost:");
@@ -38,43 +39,65 @@ void read_stream(const std::string& addr) {
 }
 
 int main(int argc, char** argv) {
+  std::string input_video("../../one_sec.mp4");
+  int base_port = 8999;
+  int num_streams = 1;
+  int width = 640;
+  int height = 368;
+  bool lossless = false;
+
+  std::string size_cmd = " -vf scale=" + 
+      std::to_string(width) + "x" + std::to_string(height);
+  std::string input_cmd = "ffmpeg -re -i " + input_video + " ";
+  std::string encoding_flags("");
+  if (lossless)
+    encoding_flags = " -crf 0 -c:v libx264rgb ";
+  std::string output_cmd = size_cmd + " -movflags faststart " + 
+      encoding_flags + " -b 90000k -an -f mpegts tcp://localhost:";
+
+  for (int i = 0; i < num_streams; ++i) {
+    int port = base_port + i;
+    std::string port_cmd = std::to_string(port) + "?listen";
+    std::string cmd = input_cmd + output_cmd + port_cmd;
+    if (fork() == 0) {
+      std::cout << cmd << std::endl;
+      system(cmd.c_str());
+      return 0;
+    }
+  }
 
 #ifdef SPAWN_THREADS
   std::vector<std::thread> threads;
-  if (argc > 1) {
-    threads.reserve(argc - 1);
-    std::string port;
-    for (int i = 1; i < argc; ++i) {
-      port = std::string(argv[i]);
-      std::string addr = addr_prefix + port + addr_postfix;
-      std::this_thread::sleep_for (std::chrono::milliseconds(100));
-      threads.emplace_back(std::move(std::thread(read_stream, addr)));
-    }
-  } else {
-    std::cout << "usage: ./ffmpeg_image [list of ports to connect]" << std::endl;
-    return 0;
+  threads.reserve(num_streams);
+  std::string port;
+  for (int i = 0; i < num_streams; ++i) {
+    int port_num = base_port + i;
+    port = std::to_string(port_num);
+    std::string addr = addr_prefix + port + addr_postfix;
+    std::this_thread::sleep_for (std::chrono::milliseconds(100));
+    threads.emplace_back(std::move(std::thread(read_stream, addr)));
   }
   for (int i = 0; i < threads.size();++i)
     threads[i].join();
 #endif
 #ifdef SPAWN_PROCESSES
-  if (argc > 1) {
-    std::string port;
-    for (int i = 1; i < argc; ++i) {
-      port = std::string(argv[i]);
-      std::string addr = addr_prefix + port + addr_postfix;
-      if (fork() == 0) {
-        read_stream(addr);
-      } 
-      std::this_thread::sleep_for (std::chrono::milliseconds(100));
-    }
-    for (int i = 1; i < argc; ++i) 
-      wait(NULL);
-  } else {
-    std::cout << "usage: ./ffmpeg_image [list of ports to connect]" << std::endl;
-    return 0;
+  std::string port;
+  for (int i = 0; i < num_streams; ++i) {
+    port = std::to_string(base_port + i);
+    std::string addr = addr_prefix + port + addr_postfix;
+    std::this_thread::sleep_for (std::chrono::milliseconds(500));
+    if (fork() == 0) {
+      std::cout << addr <<" " << i << std::endl;
+      read_stream(addr);
+      return 0;
+    } 
   }
+  for (int i = 0; i < num_streams; ++i) 
+    wait(NULL);
 #endif
+  // Wait input streams to finish
+  for (int i = 0; i < num_streams; ++i) 
+    wait(NULL);
 
   return 0;
 }
