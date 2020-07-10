fb5892cfe6650eae8aebe0814684281eccb87c77
Yucao42 <cqcy3210@gmail.com>
2020-07-10 13:56:36 -0500

    fps

--- ffmpeg/ffmpeg_image.cpp
+++ ffmpeg/ffmpeg_image.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <memory>
 #include <thread>
+#include <chrono>
 #include <mutex>
 #include <deque>
 #include <condition_variable>
@@ -14,7 +15,12 @@
 
 const std::string addr_prefix("tcp://localhost:");
 const std::string addr_postfix("?client");
+auto now = std::chrono::system_clock::now();
+auto now_ms = std::chrono::time_point_cast<std::chrono::microseconds>(now);
+long input_time = now_ms.time_since_epoch().count();
+long init_time = 0;
 int total = 0;
+double fps;
 std::mutex mu;
 
 void read_stream(const std::string& addr) {
@@ -35,11 +41,19 @@ void read_stream(const std::string& addr) {
       continue_ = cap.read(image);
       {
         std::unique_lock<std::mutex> lk(mu);
-        ++total;
+        auto now = std::chrono::system_clock::now();
+        auto now_ms = std::chrono::time_point_cast<std::chrono::microseconds>(now);
+        if (init_time == 0) {
+          init_time = now_ms.time_since_epoch().count();
+        } else {
+          long time_passed = now_ms.time_since_epoch().count() - init_time; 
+          fps = (++total) * 1.0 / time_passed * 1000000; 
+          std::cout << "FPS: " << fps << std::endl;
+        }
       }
       std::cout<< "Read one image " << addr << " count: " << total << std::endl;
       timer.tick();
-      cv::Mat wrapper = cv::Mat(image.size(), image.type(), image.data);
+      // cv::Mat wrapper = cv::Mat(image.size(), image.type(), image.data);
     }
   }
 }
@@ -52,8 +66,8 @@ int main(int argc, char** argv) {
   } else {
     num_streams = atoi(argv[1]);
   }
-  std::string input_video("../../one_sec.mp4");
-  // std::string input_video("../../sample_1080p_h264.mp4");
+  // std::string input_video("../../one_sec.mp4");
+  std::string input_video("../../sample_1080p_h264.mp4");
   int base_port = 8999;
   int width = 640;
   int height = 368;
@@ -61,7 +75,7 @@ int main(int argc, char** argv) {
 
   std::string size_cmd = " -vf scale=" + 
       std::to_string(width) + "x" + std::to_string(height);
-  std::string input_cmd = "ffmpeg -loglevel -8 -re -i " + input_video + " ";
+  std::string input_cmd = "ffmpeg -loglevel 32 -i " + input_video + " ";
   std::string encoding_flags("");
   if (lossless)
     encoding_flags = " -crf 0 -c:v libx264rgb ";
